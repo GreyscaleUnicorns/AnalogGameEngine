7b764366e052a60d934a4cc26e1d173b6c0f2d39
Nico Linder <nicolinder@icloud.com>
2017-12-16 21:59:27 +0100

    introduce dictionaries and access protected properties for sets and stacks to player

--- CardGameEngine/Entities/Player.cs
+++ CardGameEngine/Entities/Player.cs
@@ -1,7 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace CardGameEngine.Entities {
     public class Player {
         public string Name { get; private set; }
+
+        private Dictionary<string, Set> sets;
+        public ImmutableDictionary<string, Set> Sets {
+            get {
+                return sets.ToImmutableDictionary();
+            }
+        }
+
+        private Dictionary<string, Stack> stacks;
+        public ImmutableDictionary<string, Stack> Stacks {
+            get {
+                return stacks.ToImmutableDictionary();
+            }
+        }
+
+        public Player() {
+            this.sets = new Dictionary<string, Set>();
+            this.stacks = new Dictionary<string, Stack>();
+        }
     }
 }
